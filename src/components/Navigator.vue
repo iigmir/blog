3af2d80011ab975ccfc1108c29802538d7fac654
iigmir <roc120j@gmail.com>
2019-04-12 21:44:12 +0800

    Add nav

---
+++ src/components/Navigator.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="is-navbar-container">
+        <div class="is-brand">
+            <b class="is-logo">Brand</b>
+        </div>
+        <div class="is-navbar">
+            <nav>
+                <ul>
+                    <li><router-link to="/">Home</router-link></li>
+                    <li><router-link to="/about">About</router-link></li>
+                    <li><a href="#">...</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.is-navbar-container
+{
+    margin: 1.2rem;
+}
+</style>
