e37e25e6c51f6470d47ebef4605fd4a16c749091
iigmir <roc120j@gmail.com>
2019-04-18 22:33:51 +0800

    Add sc

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="about">
         <p>This is my blog...That's it.</p>
+        <a href="https://github.com/iigmir/blog" target="_blank">Source code</a>
     </div>
 </template>
