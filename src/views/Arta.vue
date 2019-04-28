e0e8e8a72d07ca407249df9ccaf8dcbbdfd0eab2
iigmir <roc120j@gmail.com>
2019-04-28 23:10:17 +0800

    Add h2

--- src/views/Arta.vue
+++ src/views/Arta.vue
@@ -1,6 +1,7 @@
 <template>
     <main>
         <article v-if="article_empty === false">
+            <h2> {{ article_info.title }} </h2>
             <vue-markdown :source="article_centent"></vue-markdown>
         </article>
     </main>
@@ -14,8 +15,9 @@ export default {
     name: "Article",
     components: { VueMarkdown },
     computed: {
-        ...mapState(["article_centent"]),
-        article_empty() { return this.article_centent === "" }
+        ...mapState(["article_centent", "contents"]),
+        article_empty() { return this.article_centent === ""; },
+        article_info()  { return this.contents.filter( a => a.id === parseInt( this.$route.params.id, 10 ) )[0]; }
     },
     methods:
     {
@@ -32,3 +34,10 @@ export default {
     beforeDestroy() { this.set_article_centent(""); }
 }
 </script>
+
+<style lang="scss" scoped>
+h2
+{
+    font-size: 2em;
+}
+</style>
