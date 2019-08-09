a8a363465419fad799ea18e06c1a418543390f6d
iigmir <roc120j@gmail.com>
2019-08-10 02:34:00 +0800

    Change router mode to fix ghpage problrm

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ import Tags from "./views/Tags.vue";
 Vue.use(Router);
 
 export default new Router({
-    mode: "history",
+    mode: "hash",
     base: process.env.BASE_URL,
     routes: [
         {
