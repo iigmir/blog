0910fa3ad7fe6c9567f44d6dbabdf9604ac6b3bf
iigmir <roc120j@gmail.com>
2019-11-23 01:11:37 +0800

    Write about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
     <main id="about">
-        <p> Hello, I am iigmir, whi is a web developer. This blog about my notes of web development. Mostly focus on front-end development, but there may have other things. </p>
+        <blockquote>
+            <p> Hello, I am a front-end developer whose brain are full of glue. </p>
+        </blockquote>
+        <p> Hello, I am iigmir, a web developer. My hobby is writing and reading. I believed and love freedom. </p>
+        <p> This blog about my notes of web development. Mostly focus on front-end development, but there may have other things. </p>
     </main>
 </template>
